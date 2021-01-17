a95d6fc14c13e5fa5816a153812e02dcd3fd45c7
Luke Blaney <git@lukeblaney.co.uk>
2021-01-17 15:55:43 +0000

    skip to next when receiver gets to end of track

--- public/receiver.js
+++ public/receiver.js
@@ -35,31 +35,50 @@ function poll(url, handleDataFunction, additionalParamFunction, cache) {
 	actuallyPoll(null);
 }
 
-function newPollInfo(data) {
-	const manager = cast.framework.CastReceiverContext.getInstance().getPlayerManager();
-	const now = data.tracks[0];
-	if (!now) return console.log("No currently playing track", data);
-	const mediaInfo = manager.getMediaInformation();
-	if (now.url == mediaInfo.contentId) return;
+function setupCastReceiver() {
+	document.body.appendChild(document.createElement('cast-media-player'));
+	const receiverContext = cast.framework.CastReceiverContext.getInstance();
+	const playerManager = receiverContext.getPlayerManager();
 
-	loadData = new cast.framework.messages.LoadRequestData();
-	loadData.currentTime = now.currentTime;
-	loadData.autoplay = true;
-	loadData.media = new cast.framework.messages.MediaInformation();
-	loadData.media.contentId = now.url;
-	loadData.media.metadata = new cast.framework.messages.MusicTrackMediaMetadata();
-	loadData.media.metadata.title = now.metadata.title;
-	loadData.media.metadata.albumName = now.metadata.album;
-	loadData.media.metadata.artist = now.metadata.artist;
-	loadData.media.metadata.contentRating = now.metadata.rating;
-	loadData.media.metadata.releaseDate = now.metadata.year;
-	loadData.media.metadata.images = [
-			{url: now.metadata.img}
-		];
-	manager.load(loadData)
-		.then(() => console.log("New track loaded"))
-		.catch(err => console.error("Can't load track", error));
+	// Ideally MEDIA_FINISHED would provide info about the track which has finished
+	// But instead, we track the current media and hope there's no race conditions
+	var currentMedia;
+	playerManager.addEventListener(cast.framework.events.EventType.MEDIA_STATUS, event => {
+		if ('media' in event.mediaStatus) currentMedia = event.mediaStatus.media;
+	});
+	playerManager.addEventListener(cast.framework.events.EventType.MEDIA_FINISHED, event => {
+
+		// Interrupted means the track was actively changed, so no need to update the server
+		if (event.endedReason == 'INTERRUPTED') return;
+		// If there's no current track, the server won't know which to skip
+		if (!currentMedia) return;
+		fetch(dataOrigin+"done?track="+encodeURIComponent(currentMedia.contentId)+"&status="+encodeURIComponent(event.endedReason), {method: 'POST'});
+	});
+	receiverContext.start();
+	poll(dataOrigin+"poll/summary", data => {
+		const now = data.tracks[0];
+		if (!now) return console.error("No currently playing track", data);
+		const mediaInfo = playerManager.getMediaInformation();
+		if (mediaInfo && now.url == mediaInfo.contentId) return;
+
+		loadData = new cast.framework.messages.LoadRequestData();
+		loadData.currentTime = now.currentTime;
+		loadData.autoplay = true;
+		loadData.media = new cast.framework.messages.MediaInformation();
+		loadData.media.contentId = now.url;
+		loadData.media.metadata = new cast.framework.messages.MusicTrackMediaMetadata();
+		loadData.media.metadata.title = now.metadata.title;
+		loadData.media.metadata.albumName = now.metadata.album;
+		loadData.media.metadata.artist = now.metadata.artist;
+		loadData.media.metadata.contentRating = now.metadata.rating;
+		loadData.media.metadata.releaseDate = now.metadata.year;
+		loadData.media.metadata.images = [
+				{url: now.metadata.img}
+			];
+		playerManager.load(loadData)
+			.catch(err => console.error("Can't load track", err));
+	});
 }
 
-cast.framework.CastReceiverContext.getInstance().start();
-poll(dataOrigin+"poll/summary", newPollInfo);
+// TODO: detect whether the page has been opened on a Google Cast device
+setupCastReceiver();

8d7b59b3a8c37cacc6665edb7481ea595b27598a
Luke Blaney <git@lukeblaney.co.uk>
2021-01-22 22:19:48 +0000

    add google cast sender logic to v3

---
+++ clientjs/cast-sender.js
@@ -0,0 +1,26 @@
+const {receiverApplicationId} = clientVariables;
+window['__onGCastApiAvailable'] = (isAvailable) => {
+
+	// I've never seen `isAvailable` to be be false, but the docs say to check for it.
+	if (!isAvailable) return;
+
+	/**
+	 * The cast library has a race condition which means occasionally it hasn't set its global variables by the time this function is called.
+	 * I haven't debugged it in detail, so just bail when it's detected and log the error
+	 * If no cast library has loaded properly, the `cast` global will be missing
+	 * If the receiver library has loaded, but not the sender, `CastContext` will be missing from `cast.framework`
+	 **/
+	if (!('cast' in window)) return console.error("Race condition: cast variable not ready");
+	if (!('CastContext' in cast.framework)) return console.error("Race condition: CastContext variable not ready");
+
+	// Instruct the cast library which receiver app to use when launched
+	cast.framework.CastContext.getInstance().setOptions({
+		receiverApplicationId
+	});
+
+	// Add a launcher button to the control panel
+	const launcher = document.createElement("google-cast-launcher");
+	const control = document.createElement("li");
+	control.appendChild(launcher);
+	document.getElementById("controls").appendChild(control);
+};
--- clientjs/v3.js
+++ clientjs/v3.js
@@ -1,4 +1,5 @@
 require("./cast-receiver");
 require("./navbar");
 require("./keyboard");
+require("./cast-sender");
 if (!clientVariables) console.error("Can't find `clientVariables` in global scope");

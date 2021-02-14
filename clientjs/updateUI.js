61312854714decd3a762341a3c09ad6f0571e3a1
Luke Blaney <git@lukeblaney.co.uk>
2021-02-14 13:21:37 +0000

    dynamically update playlist view client-side

--- clientjs/updateUI.js
+++ clientjs/updateUI.js
@@ -2,8 +2,10 @@ const pubsub = require("./pubsub");
 require("./page-loaded");
 
 function readData(data) {
-	const now = data.tracks[0] || {};
+	const tracks = data.tracks;
+	const now = tracks.shift();
 	updateNow(now);
+	updatePlaylist(tracks);
 	document.querySelectorAll("volume-control")
 		.forEach(volControl => volControl.setAttribute("volume", data.volume));
 }
@@ -14,6 +16,20 @@ function updateNow(now) {
 	document.getElementById("now_artist").firstChild.nodeValue = metadata.artist;
 	document.getElementById("now_thumb").src = metadata.thumb;
 	document.getElementById("edit").action = metadata.editurl;
+}
+
+function updatePlaylist(tracks) {
+	const playlist = document.getElementById("playlist");
+	while (playlist.firstChild) {
+		playlist.removeChild(playlist.lastChild);
+	}
+	tracks.forEach(track => {
+		const li = document.createElement("li");
+		const span = document.createElement("span");
+		span.appendChild(document.createTextNode(track.metadata.title));
+		li.appendChild(span);
+		playlist.appendChild(li);
+	});
 }
 
 pubsub.listen("ready", () => {

4c587834e58f37591058ddac1bdac7965c83bbf7
Luke Blaney <git@lukeblaney.co.uk>
2021-02-21 13:01:33 +0000

    use shadowDOM for playlist component

--- clientjs/components/playlist.js
+++ clientjs/components/playlist.js
@@ -1,61 +1,52 @@
 const pubsub = require("../pubsub");
 
-class Playlist extends HTMLOListElement {
+class Playlist extends HTMLElement {
 	constructor() {
 		super();
 
 		const component = this;
-		addGlobalStyle();
+		const shadow = component.attachShadow({mode: 'closed'});
+		const style = document.createElement('style');
+		style.textContent = `
+			ol {
+				overflow: auto;
+				margin: 0;
+				padding-top: 20px;
+				padding-bottom: 10px;
+				-webkit-overflow-scrolling: touch;
+				list-style-type: disc;
+			}
+			li {
+				font-weight: bold;
+				font-size: larger;
+				line-height: 0.9em;
+			}
+			li > span {
+				font-size: small;
+				padding: 0 1em;
+			}
+
+		`;
+		shadow.append(style);
+		const list = document.createElement("ol");
+		list.start = 2;
+		shadow.append(list);
 
 		function updatePlaylist(data) {
 			const tracks = data.tracks.slice(1); // Ignore first track as that's used by `now`
-			while (component.firstChild) {
-				component.removeChild(component.lastChild);
+			while (list.firstChild) {
+				list.removeChild(list.lastChild);
 			}
 			tracks.forEach(track => {
 				const li = document.createElement("li");
 				const span = document.createElement("span");
 				span.appendChild(document.createTextNode(track.metadata.title));
 				li.appendChild(span);
-				component.appendChild(li);
+				list.appendChild(li);
 			});
 		}
 		pubsub.listenExisting("managerData", updatePlaylist, true);
 	}
 }
 
-let globalStyleAdded = false;
-function addGlobalStyle() {
-
-	//Only ever load global style once
-	if (globalStyleAdded) return;
-
-	// Device-specific overrides
-	const globalStyle = document.createElement('style');
-
-	globalStyle.textContent = `
-		[is=track-playlist] {
-			overflow: auto;
-			margin: 0;
-			padding-top: 20px;
-			padding-bottom: 10px;
-			-webkit-overflow-scrolling: touch;
-			list-style-type: disc;
-		}
-		[is=track-playlist] li {
-			font-weight: bold;
-			font-size: larger;
-			line-height: 0.9em;
-		}
-		[is=track-playlist] li > span {
-			font-size: small;
-			padding: 0 1em;
-		}
-	`;
-
-	// Prepend the global style, so individual pages can easily override (eg to set their own background-color)
-	document.head.prepend(globalStyle);
-	globalStyleAdded = true;
-}
-
-customElements.define('track-playlist', Playlist, { extends: "ol" });
+customElements.define('track-playlist', Playlist);

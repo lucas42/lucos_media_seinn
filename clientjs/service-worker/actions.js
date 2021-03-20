a736afa2802618834876113c588ef0720edd00f0
Luke Blaney <git@lukeblaney.co.uk>
2021-03-20 23:48:10 +0000

    implement actions in service worker

--- clientjs/service-worker/actions.js
+++ clientjs/service-worker/actions.js
@@ -1,10 +1,13 @@
 const actionQueue = [];  // TODO: persist queue locally
 let timer;
+
+// TODO: only keep the most recent volume request and play/pause request in queue
 function add(request) {
 	actionQueue.push(request);
 	processQueue();
 }
 
+// TODO: process queue when device detects network change
 async function processQueue() {
 	if (timer) clearTimeout(timer);
 	while (actionQueue.length > 0) {
--- clientjs/service-worker/polling.js
+++ clientjs/service-worker/polling.js
@@ -6,16 +6,18 @@ let listeners = [];
 let pollData = {};
 
 pubsub.listenExisting("managerData", serverData => {
-	console.log("new manager data", listeners.length);
-
-	// TODO: apply modifications based on action queue
 	pollData = serverData;
-	updatePollDataWithActionsFromQueue();
+	actions.getQueue()
+		.forEach(enactAction);
+	dataChanged();
+});
+
+function dataChanged() {
 	let resolve;
 	while (resolve = listeners.shift()) {
 		resolve(getCurrentResponse());
 	}
-});
+}
 
 function getCurrentResponse() {
 	const blob = new Blob([JSON.stringify(pollData)]);
@@ -31,15 +33,55 @@ function getPoll(hashcode) {
 	});
 }
 
-function updatePollDataWithActionsFromQueue() {
-	actions.getQueue()
-		.forEach(modifyPollData);
+function enactAction(action) {
+	const url = new URL(action.url);
+	const params = new URLSearchParams(url.search);
+	const update_url = params.get("update_url");
+	const update_time = params.get("update_time");
+	if (update_url && update_time) {
+		pollData.tracks.forEach(track => {
+			if (track.url == update_url) {
+				track.currentTime = update_time;
+			}
+		});
+	}
+
+	const command = url.pathname.substring(1); // Remove leading slash from path
+	switch (command) {
+		case "volume":
+			pollData.volume = params.get("volume");
+			break;
+		case "play":
+			pollData.isPlaying = true;
+			break;
+		case "pause":
+			pollData.isPlaying = false;
+			break;
+		case "next":
+			pollData.tracks.shift();
+			break;
+		case "done":
+			pollData.tracks = pollData.tracks.filter(track => track.url !== params.get("track"));
+			break;
+		case "devices":
+			pollData.devices.forEach(device => {
+				if (device.uuid === params.get("uuid")) {
+					device.name = params.get("name");
+				}
+			});
+			break;
+		case "devices/current":
+			pollData.devices.forEach(device => {
+				device.isCurrent = (device.uuid === params.get("uuid"));
+			});
+			break;
+		default:
+			console.error("unknown action", command, params.toString());
+	}
 }
 function modifyPollData(action) {
-	switch (action.url) {
-		default:
-			console.error("unknown action", action);
-	}
+	enactAction(action);
+	dataChanged();
 }
 
 module.exports = {getPoll, modifyPollData};

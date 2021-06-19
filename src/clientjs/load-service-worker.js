d8d9562c8a891e6e1690134d0c3bb15d5b2f54a1
Luke Blaney <git@lukeblaney.co.uk>
2021-06-19 19:36:56 +0100

    bail out of service worker logic on cast devices

--- src/clientjs/load-service-worker.js
+++ src/clientjs/load-service-worker.js
@@ -1,6 +1,7 @@
 async function init() {
 	try {
 		if (!('serviceWorker' in navigator)) throw "no service worker support";
+		if ('cast' in window) throw "too taxing for cast devices, aborting";
 		const registration = await navigator.serviceWorker.register('/serviceworker-v3.js');
 		console.log('ServiceWorker registration successful with scope: ' + registration.scope);
 		registration.update();

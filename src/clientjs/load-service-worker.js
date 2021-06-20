657aa4071d569d928ea43ff499df860050a41115
Luke Blaney <git@lukeblaney.co.uk>
2021-06-20 02:26:50 +0100

    Use cast detection function for deciding whether to use service worker.
    Abstracts away `isCastReceiver` function into separate file

--- src/clientjs/load-service-worker.js
+++ src/clientjs/load-service-worker.js
@@ -1,7 +1,9 @@
+const {isCastReceiver} = require("./cast-detection");
+
 async function init() {
 	try {
 		if (!('serviceWorker' in navigator)) throw "no service worker support";
-		if ('cast' in window) throw "too taxing for cast devices, aborting";
+		if (isCastReceiver()) throw "too taxing for cast devices, aborting";
 		const registration = await navigator.serviceWorker.register('/serviceworker-v3.js');
 		console.log('ServiceWorker registration successful with scope: ' + registration.scope);
 		registration.update();
--- src/clientjs/player.js
+++ src/clientjs/player.js
@@ -1,14 +1,5 @@
 const manager = require("./manager");
-function isCastReceiver() {
-
-	// Don't even bother trying to create a cast receiver with the relevant libraries
-	if (!('cast' in window)) return false;
-	const capabilities = cast.framework.CastReceiverContext.getInstance().getDeviceCapabilities();
-
-	// If capabalities is null, then the device isn't set up for receiving casts;
-	if (capabilities == null) return false;
-	return true;
-}
+const {isCastReceiver} = require("./cast-detection");
 
 /**
  * Currently there are 3 different players included, all with a consistent API:

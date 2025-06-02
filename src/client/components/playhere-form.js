6b849d3cb6c846443de6546bbc26c7aa129bd435
Luke Blaney <git@lukeblaney.co.uk>
2025-06-02 16:47:04 +1200

    Reinstate the 'play here' button, with 'play all here' as an extra option from a dropdown

--- src/client/components/playhere-form.js
+++ src/client/components/playhere-form.js
@@ -4,7 +4,7 @@
  * The functionality here is equivalent to selecting the current device from the device overlay and also pressing play (if currently paused)
  * But this button does it in a single click without the cognative overload of thinking about which device this is or the current state
  * 
- * Clicking the button is idempotent, though it hides itself the local device is already the current one
+ * Clicking the button is idempotent, though it hides itself when the local device is already the current one
  **/
 
 import { listenExisting } from 'lucos_pubsub';
@@ -14,30 +14,59 @@ import localDevice from '../../classes/local-device.js';
 class PlayHereForm extends HTMLFormElement {
 	constructor() {
 		super();
-		const submit = document.createElement("input");
-		submit.type = "submit";
-		submit.value = "Play All Here";
-		this.append(submit);
+		const group = document.createElement("div");
+		group.classList.add("control-group");
+
+		const extracontrols = document.createElement("div");
+		extracontrols.classList.add("extra-controls");
+		group.append(extracontrols);
+
+		const playhere = document.createElement("input");
+		playhere.type = "submit";
+		playhere.value = "Play Here";
+		playhere.classList.add("primary-control");
+		group.append(playhere);
 		this.addEventListener('submit', async event => {
+			event.preventDefault();
+			await put("v3/current-device", localDevice.getUuid());
+			await put("v3/is-playing", "true");
+		});
+		const playallhere = document.createElement("input");
+		playallhere.type = "button";
+		playallhere.value = "Play All Here";
+		extracontrols.append(playallhere);
+		playallhere.addEventListener('click', async event => {
 			event.preventDefault();
 			await put("v3/current-device", localDevice.getUuid());
 			await put("v3/current-collection", "all");
 			await put("v3/is-playing", "true");
 		});
+		const showmore = document.createElement("input");
+		showmore.type = "button";
+		showmore.value = "↓";
+		showmore.classList.add("show-more");
+		showmore.addEventListener("click", () => {
+			if (group.dataset.expanded == "true") {
+				delete group.dataset.expanded;
+				showmore.value = "↓";
+			} else {
+				group.dataset.expanded = "true";
+				showmore.value = "ꜛ";
+			}
+		})
+		group.append(showmore);
 		listenExisting("managerData", data => {
 			if (localDevice.isCurrent()) {
-				submit.classList.add("hide");
+				group.classList.add("hide");
 			} else {
-				submit.classList.remove("hide");
+				group.classList.remove("hide");
 			}
 		});
 		const style = document.createElement('style');
 		style.textContent = `
-			.hide {
-				display:none;
-			}
 		`;
-		this.prepend(style);
+		this.append(style);
+		this.append(group);
 	}
 }
 

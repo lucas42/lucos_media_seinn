682ef3534ad9f115e805c26e21e22f98628f80fa
Luke Blaney <git@lukeblaney.co.uk>
2025-08-17 20:53:31 +0100

    get client-variables from cache, rather than over network

--- src/utils/init-variables.js
+++ src/utils/init-variables.js
@@ -2,7 +2,7 @@ import { init as initManager } from '../utils/manager.js';
 import { init as initHeaders } from '../utils/media-headers.js';
 
 (async function _init() {
-	const response = await fetch('/client-variables.json');
+	const response = await caches.match(new Request("/client-variables.json"))
 	const { mediaManager, apiKey, mediaCreds } = await response.json();
 	initManager(mediaManager, apiKey);
 	initHeaders(mediaCreds);

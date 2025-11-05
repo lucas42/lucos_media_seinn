7877666c67635f8d0ceb5afad0b6d4a6e77e2536
Luke Blaney <git@lukeblaney.co.uk>
2025-11-05 21:58:58 +0000

    if client-variables.json isn't found in the cache, fallback to network request

--- src/utils/init-variables.js
+++ src/utils/init-variables.js
@@ -2,7 +2,12 @@ import { init as initManager } from '../utils/manager.js';
 import { init as initHeaders } from '../utils/media-headers.js';
 
 (async function _init() {
-	const response = await caches.match(new Request("/client-variables.json"))
+	const request = new Request("/client-variables.json");
+	let response = await caches.match(request);
+	if (!response) {
+		console.warn("client-variables.json not found in cache; falling back to network");
+		response = await fetch(request);
+	}
 	const { mediaManager, apiKey, mediaCreds } = await response.json();
 	initManager(mediaManager, apiKey);
 	initHeaders(mediaCreds);
